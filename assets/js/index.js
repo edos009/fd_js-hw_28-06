50b1a8d7afc7e85ab277ce7a5ec9337e275b5024
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-07-01 14:35:28 +0300

    Edit fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,20 +7,11 @@ for (const btn of btns) {
 }
 
 function btnHandler({ target }) {
-  if (
-    target.dataset.direction === "column" ||
-    target.dataset.direction === "column-reverse"
-  ) {
-    getSizeWidth("100%");
-  } else {
-    getSizeWidth("50px");
+  for (const btn of btns) {
+    btn.classList.remove("add-color-btn");
   }
+
+  target.classList.add("add-color-btn");
 
   boxItems.style.flexDirection = target.dataset.direction;
-}
-
-function getSizeWidth(size) {
-  for (const item of items) {
-    size === "100%" ? (item.style.width = "100%") : (item.style.width = "50px");
-  }
 }
